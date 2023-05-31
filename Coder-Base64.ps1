168ec064313d82a636b226046f9450bc4ce1c651
Chen-HuangRen <tn918419@gmail.com>
2023-05-31 10:24:04 +0800

    Create Base64 coder

---
+++ Coder-Base64.ps1
@@ -0,0 +1,24 @@
+# usage: Base64-Coder Encode-File ${Source}
+# usage: Base64-Coder Decode-File ${Source}
+$ActionList = @("encode-file", "decode-file")
+
+$Action = $args[0]
+$Source = $args[1]
+# $Target = $args[2]
+
+# Filter invalid actions
+if ( $Action ) { $Action = $Action.ToLower() } else { $Action = "" }
+if ( $Action -eq "" -or ( $ActionList.IndexOf($Action) -eq -1 )) 
+  { 
+    Write-Error "Error: no action to perform" ;
+    Exit 1 ;
+  }
+
+if ( $Action -eq "Encode-File" )
+  {
+    [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($(Get-Content -Raw $Source))) | Write-Output
+  }
+if ( $Action -eq "Decode-File" )
+  {
+    [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($(Get-Content $Source))) | Write-Output
+  }
